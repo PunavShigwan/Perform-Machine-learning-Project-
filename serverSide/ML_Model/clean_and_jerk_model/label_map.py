2b176fa379f01d6a40a386a3042c6c89e06234ad
Your Name <your.email@example.com>
2026-01-14 16:51:48 +0530

    Add extraction and evaluation scripts for clean and jerk model; update .gitignore and add necessary data files

---
+++ serverSide/ML_Model/clean_and_jerk_model/label_map.py
@@ -0,0 +1,11 @@
+LABEL_MAP = {
+    0: "P1_setup",
+    1: "P2_first_pull",
+    2: "P3_second_pull",
+    3: "P4_catch",
+    4: "P5_recovery",
+    5: "P6_dip",
+    6: "P7_drive",
+    7: "P8_lockout",
+    8: "P9_finish"
+}
